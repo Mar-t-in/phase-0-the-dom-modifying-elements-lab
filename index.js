47522582eddd27463ceee5fd40561c547fa0bab1
Martin Kamau <martinkimu09@gmail.com>
2022-09-05 09:12:44 +0300

    done did

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+//removing #main
+const main = document.querySelector('#main');
+main.remove();
+
+//
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Martin is the champion';
